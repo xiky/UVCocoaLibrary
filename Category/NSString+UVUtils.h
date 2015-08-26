b1b2d34a03d6efca2d248ae618ed3da9e6618e3a
jayson.chen <jayson.chen@abc360.com>
2015-08-26 15:08:13 +0800

    NSString  add regex valid

---
+++ Category/NSString+UVUtils.h
@@ -0,0 +1,13 @@
+//
+//  NSString+UVUtils.h
+//  UVCocoaLibrary
+//
+//  Created by chenjiaxin on 15/8/26.
+//  Copyright (c) 2015年 Uniview. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (UVUtils)
+
+@end
---
+++ Category/NSString+UVUtils.m
@@ -0,0 +1,13 @@
+//
+//  NSString+UVUtils.m
+//  UVCocoaLibrary
+//
+//  Created by chenjiaxin on 15/8/26.
+//  Copyright (c) 2015年 Uniview. All rights reserved.
+//
+
+#import "NSString+UVUtils.h"
+
+@implementation NSString (UVUtils)
+
+@end
