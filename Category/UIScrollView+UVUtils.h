5ea0d2d7bb2e135eac66d0e0783d837c81594fbd
jayson.chen <jayson.chen@abc360.com>
2015-09-01 17:10:51 +0800

    uiscrollview utils

---
+++ Category/UIScrollView+UVUtils.h
@@ -0,0 +1,14 @@
+//
+//  UIScrollView+UVUtils.h
+//  UVCocoaLibrary
+//
+//  Created by chenjiaxin on 15/9/1.
+//  Copyright (c) 2015年 Uniview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (UVUtils)
+
+//- (void)addKeyBoard
+@end
---
+++ Category/UIScrollView+UVUtils.m
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+UVUtils.m
+//  UVCocoaLibrary
+//
+//  Created by chenjiaxin on 15/9/1.
+//  Copyright (c) 2015年 Uniview. All rights reserved.
+//
+
+#import "UIScrollView+UVUtils.h"
+
+@implementation UIScrollView (UVUtils)
+
+@end
