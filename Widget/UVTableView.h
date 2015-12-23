3d756193d9cb51f7b24d6f142ce040bc4da4332b
jayson.chen <jayson.chen@abc360.com>
2015-12-23 19:29:11 +0800

    增加UVTableView

---
+++ Widget/UVTableView.h
@@ -0,0 +1,52 @@
+//
+//  UVTableView.h
+//  testTableview
+//
+//  Created by chenjiaxin on 15/12/23.
+//  Copyright © 2015年 chenjaixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class UVTableView;
+
+typedef NS_ENUM(NSInteger, UV_TABLEVIEW_TYPE) {
+    UV_TABLEVIEW_TYPE_EMPTY = 1,
+    UV_TABLEVIEW_TYPE_FAILUREVIEW
+};
+
+/**
+ *  视图点击事件回调
+ *
+ *  @param tableView UVtableview
+ *  @param type      UV_TABLEVIEW_TYPE
+ */
+typedef void (^errorViewClickListener)(UVTableView *tableView, UV_TABLEVIEW_TYPE type);
+
+
+@interface UVTableView : UITableView
+/**
+ *  数据为空视图
+ */
+@property (nonatomic,strong) UIView *emptyView;
+/**
+ *  数据加载失败视图
+ */
+@property (nonatomic,strong) UIView *failureView;
+/**
+ *  视图点击事件
+ */
+@property (nonatomic,strong) errorViewClickListener errorViewClickEvent;
+
+/**
+ *  控制是否显示数据为空视图
+ *
+ *  @param status_ BOOL
+ */
+- (void)showEmptyView:(BOOL)status_;
+/**
+ *  控制是否显示错误视图
+ *
+ *  @param status_ BOOL
+ */
+- (void)showFailureView:(BOOL)status_;
+@end
---
+++ Widget/UVTableView.m
@@ -0,0 +1,119 @@
+//
+//  UVTableView.m
+//  testTableview
+//
+//  Created by chenjiaxin on 15/12/23.
+//  Copyright © 2015年 chenjaixin. All rights reserved.
+//
+
+#import "UVTableView.h"
+
+@interface UVTableView ()
+@property (nonatomic,assign) UITableViewCellSeparatorStyle oldSepStyle;
+@end
+
+@implementation UVTableView
+
+- (void)setEmptyView:(UIView *)emptyView_
+{
+    if(_emptyView != nil)
+    {
+        [_emptyView removeFromSuperview];
+    }
+    _emptyView = emptyView_;
+    
+    _emptyView.userInteractionEnabled = YES;
+    _emptyView.hidden = YES;
+    [self addSubview:_emptyView];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onEmptyViewTap:)];
+    [_emptyView addGestureRecognizer:tap];
+}
+
+- (void)setFailureView:(UIView *)failureView_
+{
+    if(_failureView != nil)
+    {
+        [_failureView removeFromSuperview];
+    }
+    _failureView = failureView_;
+    _failureView.hidden = YES;
+    _failureView.userInteractionEnabled = YES;
+    
+    [self addSubview:_failureView];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onFailureViewTap:)];
+    [_failureView addGestureRecognizer:tap];
+}
+
+
+- (void)showEmptyView:(BOOL)status_
+{
+    if(_emptyView == nil)return;
+    if(status_)
+    {
+        [_emptyView setHidden:NO];
+        if(self.separatorStyle != UITableViewCellSeparatorStyleNone)
+        {
+            _oldSepStyle = self.separatorStyle;
+            self.separatorStyle = UITableViewCellSeparatorStyleNone;
+        }
+    }
+    else
+    {
+        [_emptyView setHidden:YES];
+        if(_oldSepStyle != UITableViewCellSeparatorStyleNone)
+        {
+            self.separatorStyle = _oldSepStyle;
+        }
+    }
+}
+
+- (void)showFailureView:(BOOL)status_
+{
+    if(_failureView == nil)return;
+    if(status_)
+    {
+        [_failureView setHidden:NO];
+        if(self.separatorStyle != UITableViewCellSeparatorStyleNone)
+        {
+            _oldSepStyle = self.separatorStyle;
+            self.separatorStyle = UITableViewCellSeparatorStyleNone;
+        }
+    }
+    else
+    {
+        [_failureView setHidden:YES];
+        if(_oldSepStyle != UITableViewCellSeparatorStyleNone)
+        {
+            self.separatorStyle = _oldSepStyle;
+        }
+    }
+}
+
+#pragma mark - event
+- (void)onEmptyViewTap:(UIGestureRecognizer*)tap_
+{
+    if(_errorViewClickEvent != nil)
+    {
+        _errorViewClickEvent(self, UV_TABLEVIEW_TYPE_EMPTY);
+    }
+}
+
+- (void)onFailureViewTap:(UIGestureRecognizer*)tap_
+{
+    if(_errorViewClickEvent != nil)
+    {
+        _errorViewClickEvent(self, UV_TABLEVIEW_TYPE_FAILUREVIEW);
+    }
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
