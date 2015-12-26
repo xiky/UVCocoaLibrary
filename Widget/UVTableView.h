b9106ed4a5f5845217c85bb47bc5c3f2de53cb32
jayson.chen <jayson.chen@abc360.com>
2015-12-26 17:06:06 +0800

    增加是否自动显示空视图参数

--- Widget/UVTableView.h
+++ Widget/UVTableView.h
@@ -37,6 +37,18 @@ typedef void (^errorViewClickListener)(UVTableView *tableView, UV_TABLEVIEW_TYPE
  *  视图点击事件
  */
 @property (nonatomic,strong) errorViewClickListener errorViewClickEvent;
+/**
+ *  标记是否在加载中 默认为NO  由用户自行控制。加载中状态不会显示空视图.
+ */
+@property (nonatomic,assign) BOOL isLoading;
+/**
+ *  标记当前加载失败了 默认为NO，由用户自行控制。设置这个状态为YES，当前tableview记录为空，则会显示失败视图
+ */
+@property (nonatomic,assign) BOOL isFailured;
+/**
+ *  是否自动检测显示空视图 默认为YES
+ */
+@property (nonatomic) BOOL autoShowEmptyView;
 
 /**
  *  控制是否显示数据为空视图
--- Widget/UVTableView.m
+++ Widget/UVTableView.m
@@ -13,7 +13,48 @@
 @end
 
 @implementation UVTableView
+- (void)initData
+{
+    _autoShowEmptyView = YES;
+    _isLoading = NO;
+    _isFailured = NO;
+}
+#pragma mark - rewrite
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if(self = [super initWithCoder:aDecoder])
+    {
+        [self initData];
+    }
+    return self;
+}
+- (id)initWithFrame:(CGRect)frame
+{
+    if(self = [super initWithFrame:frame])
+    {
+        [self initData];
+    }
+    return self;
+}
+- (id)initWithFrame:(CGRect)frame style:(UITableViewStyle)style
+{
+    if(self = [super initWithFrame:frame style:style])
+    {
+        [self initData];
+    }
+    return self;
+}
+- (void)reloadData
+{
+    [super reloadData];
+    //不是自动显示空视图 或 当前已经标识加载失败了，则不处理
+    if(!_autoShowEmptyView || _isFailured)return;
 
+    BOOL empty = [self emptyDataByTableview];
+    [self showEmptyView:empty];
+}
+
+#pragma mark - public
 - (void)setEmptyView:(UIView *)emptyView_
 {
     if(_emptyView != nil)
@@ -48,20 +89,24 @@
 
 - (void)showEmptyView:(BOOL)status_
 {
+    //加载中 不显示空视图
     if(_emptyView == nil)return;
     if(status_)
     {
-        if(!_emptyView.hidden)return;
+        [self showFailureView:NO];
+        //已经显示则不处理
+        if(_emptyView.hidden == NO)return;
         [_emptyView setHidden:NO];
         if(self.separatorStyle != UITableViewCellSeparatorStyleNone)
         {
             _oldSepStyle = self.separatorStyle;
             self.separatorStyle = UITableViewCellSeparatorStyleNone;
         }
+        [self scrollsToTop];
     }
     else
     {
-        if(_emptyView.hidden)return;
+        if(_emptyView.hidden == YES)return;
         [_emptyView setHidden:YES];
         if(_oldSepStyle != UITableViewCellSeparatorStyleNone)
         {
@@ -75,20 +120,87 @@
     if(_failureView == nil)return;
     if(status_)
     {
+        [self showEmptyView:NO];
+        if(_failureView.hidden == NO)return;
+        
         [_failureView setHidden:NO];
         if(self.separatorStyle != UITableViewCellSeparatorStyleNone)
         {
             _oldSepStyle = self.separatorStyle;
             self.separatorStyle = UITableViewCellSeparatorStyleNone;
         }
+        [self scrollsToTop];
     }
     else
     {
+        if(_failureView.hidden == YES)return;
         [_failureView setHidden:YES];
         if(_oldSepStyle != UITableViewCellSeparatorStyleNone)
         {
             self.separatorStyle = _oldSepStyle;
         }
+    }
+}
+
+- (void)setIsLoading:(BOOL)isLoading_
+{
+    if(_isLoading == isLoading_)return;
+    _isLoading = isLoading_;
+    //加载中，则不显示空视图
+    if(_isLoading)
+    {
+        [self showEmptyView:NO];
+    }
+    
+}
+- (void)setIsFailured:(BOOL)isError_
+{
+    if(_isFailured == isError_)return;
+    _isFailured = isError_;
+    if(isError_)
+    {
+        BOOL empty = [self emptyDataByTableview];
+        if(empty)
+        {
+            [self showFailureView:YES];
+        }
+    }
+    else
+    {
+        [self showFailureView:NO];
+    }
+    
+}
+
+#pragma mark - private
+/**
+ *  检测当前tableview的数据源是否为空记录  满足section为1，且row为空
+ *
+ *  @return BOOL
+ */
+- (BOOL)emptyDataByTableview
+{
+    NSInteger sections = 1;
+    if([self.dataSource respondsToSelector:@selector(numberOfSectionsInTableView:)])
+    {
+        sections = [self.dataSource numberOfSectionsInTableView:self];
+    }
+    NSInteger row = 0;
+    if(sections > 0)
+    {
+        if([self.dataSource respondsToSelector:@selector(tableView:numberOfRowsInSection:)])
+        {
+            row = [self.dataSource tableView:self numberOfRowsInSection:0];
+        }
+    }
+
+    if(sections <= 1 && row == 0)
+    {
+        return YES;
+    }
+    else
+    {
+        return NO;
     }
 }
 
